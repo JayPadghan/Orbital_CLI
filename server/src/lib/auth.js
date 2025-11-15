b579bd8f97f87080cf0cfd04452de464bcff5d96
JayPadghan <padghanjay911@gmail.com>
2025-11-16 04:59:56 +0530

    03_device flow 1

--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -1,4 +1,5 @@
 import {betterAuth} from "better-auth";
+import { deviceAuthorization } from "better-auth/plugins"; 
 import {prismaAdapter} from "better-auth/adapters/prisma"
 import prisma from "./db.js";
 
@@ -8,6 +9,12 @@ export const auth = betterAuth({
     }),
     basePath:"/api/auth",
     trustedOrigins:["http://localhost:3000"],
+    plugins: [ 
+        deviceAuthorization({ 
+        expiresIn: "30m", 
+        interval: "5s",  
+        }), 
+    ],
     socialProviders: { 
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID, 
